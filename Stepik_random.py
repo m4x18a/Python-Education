a68ab9ddc1d7843190a4c3d8784fbbe4b1b5bcd7
Maxim Babukhin <m4x18a@gmail.com>
2021-10-01 19:39:44 +0300

    Method change from randint to uniform

--- Stepik_random.py
+++ Stepik_random.py
@@ -42,12 +42,13 @@ def generate_index():
 {−2≤x≤2; −2≤y≤2; x^3+y^4+2≥0; 3x+y^2≤2}"""
 
 import random
+
 n = 10**6       # количество испытаний
 k = 0
 s0 = 16
 for _ in range(n):
-    x = random.randint(-2, 2)
-    y = random.randint(-2, 2)
+    x = random.uniform(-2, 2)
+    y = random.uniform(-2, 2)
     if x**3 + y**4 + 2 >= 0 and 3*x + y**2 <= 2:
         k += 1
 print((k/n)*s0)
--- tempFile.py
+++ tempFile.py
@@ -4,8 +4,8 @@ n = 10**6       # количество испытаний
 k = 0
 s0 = 16
 for _ in range(n):
-    x = random.randint(-2, 2)
-    y = random.randint(-2, 2)
+    x = random.uniform(-2, 2)
+    y = random.uniform(-2, 2)
     if x**3 + y**4 + 2 >= 0 and 3*x + y**2 <= 2:
         k += 1
 print((k/n)*s0)
